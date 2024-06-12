94cfbd11db4423d5d2819640e21e03944f368440
Spook <lhdylan@hotmail.com>
2024-06-12 12:56:16 +0200

    refactor: start app from beginning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,10 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet, 
-    CommonModule
-  ],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pokedex';
-
-  constructor(private apiService: ApiService) {
-    this.apiService.getPokemons().subscribe((res: any) => console.log(res));
-    // this.apiService.getPokemon().subscribe((res: any) => console.log(res));
-  }
-currentPokemonUrl: any = {};
-CurrentPokemonUrlSpriteOne : string = '';
-  getInputName(name: string) {
-    data: this.apiService.getPokemon(name).subscribe((res: any) => {
-      this.currentPokemonUrl = res;
-      this.CurrentPokemonUrlSpriteOne = this.currentPokemonUrl.sprites.front_default;
-    });
-  }
 }
